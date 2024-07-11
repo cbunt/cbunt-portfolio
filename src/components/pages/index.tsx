132aa4a842cc24dbcaa62921c98f998ff18f86fb
cass bunting <99060219+cbunt@users.noreply.github.com>
2024-07-11 04:19:00 +0000

    add personal portrait

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -4,6 +4,8 @@ import FencedMarkdown from '../core/fenced-markdown';
 import CustomTooltip, { StyledTooltip } from '../core/tooltip';
 import styled from 'styled-components';
 
+import portrait from 'public/portrait.png';
+
 const ExtraStyledTooltip = styled(StyledTooltip)`
     font-size: 1.5rem;
     width: max-content;
@@ -25,12 +27,14 @@ const content = /* md */`
 # Welcome to <Hover tooltipContent="cass bunting's">my</Hover> Portfolio Website
 
 \`\`\`article
+<img src="${portrait}" width="256" style="float: left; margin: 1rem; margin-top: 0; border-radius: 5px;" />
+
 ## About the Site
 
 This website is my effort to collect what I've worked on so far in one place.
+It was created from the ground up in react and styled-components. A few
 I'm still adding new projects and linking old projects that currently live in
-other places, so the site will change. Maybe I'll even add a picture of myself! 
-Check back soon for even more samples of my work.
+other places, so the site will change.
 
 
 ## About <Hover tooltipContent="cass bunting">Me</Hover>
@@ -40,14 +44,15 @@ technical artist, and (now, with this site) web developer in Vancouver, BC. I'm
 excited about real time rendering, developing useful, streamlined technical 
 tools for other artists and developers, and creating unique interactive experiences.
 
-My experience includes designing water surfaces through voronoi tesselation for 
-a game about Metis history, extensive work with Unity, and everything else you 
+My experience includes designing water surfaces through voronoi noise for 
+a game about Metis history, creating tools and workflows for Unity, and everything you 
 see on this very website. I love to work with stylized graphics, procedural 
 generation, and especially combinations of the two. During my recently-completed 
 studies at the University of British Columbia, I honed my skills with and developed
-my passion for both low- and high-  level programming—from functional languages to 
+my passion for both low- and high- level programming—from functional languages to 
 assembly. I also have significant experience working outside of tech. I worked just 
-about every position in restaurants for about six years, and in education for four.
+about every position in restaurants for six years, and in education, particularly
+teen leadership building, for four.
 
 Aside from working with graphics, my overwhelmingly numerous hobbies include
 playing music, hiking, cooking, reading about history and social theory,
