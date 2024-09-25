ecabf9e3ca576bb89603ac0e870a62bc560e8827
cass bunting <99060219+cbunt@users.noreply.github.com>
2024-09-25 18:39:12 +0000

    description update

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -29,25 +29,17 @@ const content = /* md */`
 \`\`\`article
 <img src="${portrait}" width="256" style="float: left; margin: 1rem; margin-top: 0; border-radius: 5px;" />
 
-## About the Site
-
-This website is my effort to collect what I've worked on so far in one place.
-It was created from the ground up in react and styled-components. A few
-I'm still adding new projects and linking old projects that currently live in
-other places, so the site will change.
-
-
 ## About <Hover tooltipContent="cass bunting">Me</Hover>
 
 I'm <Hover tooltipContent="me">Cass Bunting</Hover>, a graphics-programmer,
-technical artist, and (now, with this site) web developer in Vancouver, BC. I'm
+technical artist, and (now, with this site) web developer based in Vancouver, BC. I'm
 excited about real time rendering, developing useful, streamlined technical 
 tools for other artists and developers, and creating unique interactive experiences.
 
 My experience includes designing water surfaces through voronoi noise for 
 a game about Metis history, creating tools and workflows for Unity, and everything you 
 see on this very website. I love to work with stylized graphics, procedural 
-generation, and especially combinations of the two. During my recently-completed 
+generation, and combinations of the two. During my recently-completed 
 studies at the University of British Columbia, I honed my skills with and developed
 my passion for both low- and high- level programming—from functional languages to 
 assembly. I also have significant experience working outside of tech. I worked just 
@@ -57,6 +49,12 @@ teen leadership building, for four.
 Aside from working with graphics, my overwhelmingly numerous hobbies include
 playing music, hiking, cooking, reading about history and social theory,
 creative writing, and—of course—playing video games.
+
+## About the Site
+
+This website is my effort to present my knowledge and work in a unified way.
+It was made from the ground up in Typescript and React, with realtime samples 
+created with WebGPU. Its source code can be found at <https://github.com/cbunt/cbunt-portfolio>.
 
 \`\`\`
 `;
