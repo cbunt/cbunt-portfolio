ce7e0b5502139547bb3bf1c5533e60ecacc55d89
cbunt <cass@cbunt.ing>
2024-12-03 20:08:26 +0000

    ladle config

---
+++ .ladle/config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('@ladle/react').UserConfig} */
+export default {
+    addons: {
+        a11y: {
+            enabled: true,
+        },
+        theme: {
+            enabled: true,
+            defaultState: "dark",
+        },
+    }
+};
